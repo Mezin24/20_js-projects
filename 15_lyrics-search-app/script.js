29036c8fcbade540f7232d73406e033357e2c3d6
Pavel Mezencev <mezencev24@mail.ru>
2022-05-30 00:14:32 +0300

    add UI 16

--- 15_lyrics-search-app/script.js
+++ 15_lyrics-search-app/script.js
@@ -11,7 +11,8 @@ async function searchSongs(term) {
   const res = await fetch(`${apiURL}/suggest/${term}`);
   const data = res.json();
 
-  return data;
+  console.log(data);
+  // return data;
 }
 
 form.addEventListener('submit', (e) => {
@@ -25,19 +26,3 @@ form.addEventListener('submit', (e) => {
     searchSongs(searctTerm);
   }
 });
-
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'shazam.p.rapidapi.com',
-    'X-RapidAPI-Key': '800d1c77edmsh142906e4c64c9efp1dacc1jsn17c97fdb6965',
-  },
-};
-
-fetch(
-  'https://shazam.p.rapidapi.com/search?term=love&locale=en-US&offset=0&limit=20',
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
